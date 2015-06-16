215b025c538eceff6b87307ce58b16c6235521d8
LatteCom <redeye217@gmail.com>
2015-06-17 04:27:50 +0900

    search graph2

--- avengers/public/javascripts/graph.js
+++ avengers/public/javascripts/graph.js
@@ -1,13 +1,8 @@
 $(function(){
 
 	$('.hexagon').each(function(index, el) {
-		console.log("meme");
-		console.log(el);
-		console.log(index);
-		console.log($(this));
 		var positive = [$(this).children('input[name="fun"]').val()*1,$(this).children('input[name="benefit"]').val()*1,0,0,0,$(this).children('input[name="grade"]').val()*1];
 		var negative = [0,0,$(this).children('input[name="homework"]').val()*1,$(this).children('input[name="teamplay"]').val()*1,$(this).children('input[name="difficulty"]').val()*1,0];
-		console.log("merong");
 		var radarChartData = {
 			labels: ["Interesting", "Benefit", "Assignment", "Team project", "Difficulty", "Grade"],
 			datasets: [
@@ -39,5 +34,41 @@ $(function(){
 				responsive: true
 			});
 	});
-	console.log("mu?");
+	
+	$('.tabs li').on('click', function(event) {
+		$('.hexagon').each(function(index, el) {
+		var positive = [$(this).children('input[name="fun"]').val()*1,$(this).children('input[name="benefit"]').val()*1,0,0,0,$(this).children('input[name="grade"]').val()*1];
+		var negative = [0,0,$(this).children('input[name="homework"]').val()*1,$(this).children('input[name="teamplay"]').val()*1,$(this).children('input[name="difficulty"]').val()*1,0];
+		var radarChartData = {
+			labels: ["Interesting", "Benefit", "Assignment", "Team project", "Difficulty", "Grade"],
+			datasets: [
+				{
+					label: "My First dataset",
+					fillColor: "blue",//"rgba(220,220,220,0.2)",
+					strokeColor: "blue",//"rgba(220,220,220,1)",
+					pointColor: "blue",//"rgba(220,220,220,1)",
+					pointStrokeColor: "#fff",
+					pointHighlightFill: "#fff",
+					pointHighlightStroke: "blue",//"rgba(220,220,220,1)",
+					data: positive//[65,59,0,0,0,90]
+				},
+				{
+					label: "My Second dataset",
+					fillColor: "red",//"rgba(151,187,205,0.2)",
+					strokeColor: "red",//"rgba(151,187,205,1)",
+					pointColor: "red",//"rgba(151,187,205,1)",
+					pointStrokeColor: "#fff",
+					pointHighlightFill: "#fff",
+					pointHighlightStroke: "red",//"rgba(151,187,205,1)",
+					data: negative//[0,0,27,19,96,0]
+				}
+			]
+		};
+
+		var ctx = el.getContext("2d");	/* 안됨 */
+		var newChart = new Chart(ctx).Radar(radarChartData, {
+				responsive: true
+			});
+	});
+	});
 });
